2687131b294f148fb048ed348052e88fb4db3a36
nemanjagajic <nemanja.gajicru96@gmail.com>
2020-03-08 19:03:21 +0100

    minor 18n changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import i18n from '../i18n'
 
 const Navbar = props => {
   return (
     <div className={'navbar'}>
       <div className={'left'}>
-        <div className={'item'}>Home</div>
+        <div className={'item'}>{i18n.t('navbar.home')}</div>
       </div>
       <div className={'right'}>
-        <div className={'item'} onClick={props.handleLogout}>Logout</div>
+        <div className={'item'} onClick={props.handleLogout}>{i18n.t('navbar.logout')}</div>
       </div>
     </div>
   )
