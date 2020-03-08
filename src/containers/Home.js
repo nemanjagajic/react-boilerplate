272d3eeda7f992eafd9cc09d56d7e861554ef415
BokiGaja <bgajic93@gmail.com>
2020-03-08 18:49:53 +0100

    added i18n

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux'
 import { logOut } from "../store/auth/authActions";
 import Navbar from "../components/Navbar";
+import i18n from '../i18n'
 
 const Home = () => {
   const dispatch = useDispatch()
@@ -14,7 +15,7 @@ const Home = () => {
     <div>
       <Navbar handleLogout={handleLogout}/>
       <div className={'content-wrapper'}>
-        <div className={'title'}>Home page</div>
+        <div className={'title'}>{i18n.t('homePage')}</div>
       </div>
     </div>
   );
